80cea57a84fd04fa92893d6484409b0a010ec38c
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2024-01-17 13:26:21 +1100

    restart fix

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -23,7 +23,7 @@ export default function Quiz() {
   }
   const isQuizComplete = userAnswers.length === totalDisplayQuestions;
   function handleRestart() {
-    setStartQuiz(true);
+    setStartQuiz(false);
     setUserAnswers([]);
   }
 
