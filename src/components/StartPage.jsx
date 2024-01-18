157728d030056b2a5f2f76c4e47211fce9b87570
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2024-01-18 13:55:42 +1100

    question timer fix 2

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -14,24 +14,26 @@ export default function StartPage({ onStart }) {
 
   return (
     <div className=" text-white">
-      <label className=" text-xl">Number of questions: </label>
-      <Slider
-        color="success"
-        min={3}
-        size="lg"
-        valueLabelDisplay="on"
-        max={QUESTIONS.length}
-        value={configValues.numberOfQuestions}
-        onChange={(e) => handleChange("numberOfQuestions", e.target.value)}
-      />
-      <div className=" flex justify-between">
-        <label className=" text-xl ">Per Question Time(in seconds) :</label>
+      <div className=" flex justify-between my-3">
+        <label className=" text-xl">Number of questions: </label>
+        <Slider
+          color="success"
+          min={3}
+          size="lg"
+          valueLabelDisplay="on"
+          max={QUESTIONS.length}
+          value={configValues.numberOfQuestions}
+          onChange={(e) => handleChange("numberOfQuestions", e.target.value)}
+        />
+      </div>
+      <div className=" flex justify-between my-3">
+        <label className=" text-xl me-3">Per Question Time(in seconds) :</label>
         <Slider
           color="success"
           min={5}
           size="lg"
           valueLabelDisplay="on"
-          max={30}
+          max={60}
           value={configValues.time}
           onChange={(e) => handleChange("time", e.target.value)}
         />
